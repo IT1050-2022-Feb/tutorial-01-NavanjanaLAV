79923a10119b62ade9be6160cc4ce7360510bfd7
NavanjanaLAV <127643853+NavanjanaLAV@users.noreply.github.com>
2023-03-12 06:16:11 +0000

    completed.

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+  int x,y;
+  float average;
+  printf("Enter mark of subject 1 : ");
+  scanf("%d",&x);
+
+  printf("Enter mark of subject 2 : ");
+  scanf("%d",&y);
+
+average=(x+y)/2.0;
+
+  printf("Avarage of marks  :%.2f",average);
+  
   
   return 0;
 }
