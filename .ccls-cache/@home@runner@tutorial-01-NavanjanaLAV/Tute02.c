54d59da90b452e123fe4e2dfa4d0c42aac8ed326
NavanjanaLAV <127643853+NavanjanaLAV@users.noreply.github.com>
2023-03-12 06:52:30 +0000

    complete  tute2

---
+++ .ccls-cache/@home@runner@tutorial-01-NavanjanaLAV/Tute02.c
@@ -0,0 +1,39 @@
+/*Exercise 2 - Selection
+
+Write a program to calculate the amount to be paid for a rented vehicle.
+
+•	Input the distance the van has travelled
+•	The first 30 km is at a rate of 50/= per km.
+•	The remaining distance is calculated at the rate of 40/= per km.
+
+
+e.g.
+
+Distance -> 20
+Amount = 20 x 50 = 1000
+
+
+Distance -> 50
+Amount = 30 x 50 + (50-30) x 40 = 2300*/
+
+#include <stdio.h>
+
+int main() {
+  int distance;
+  float amount;
+  printf("Enter distance the van has travelled(km) ");
+  scanf("%d",distance);
+
+  if(distance<=30)
+    amount= distance * 50;
+  else if(distance>30)
+    amount= (30*50)+ 
+     (distance-30)*60;
+
+  printf("Total Amount : 
+   %.2f",amount);
+
+
+  
+  return 0;
+}
